8ce1566da852cc0150b4bfe69b9f310da4da6629
varsha.m.nagaraj <varsha.m.nagaraj@accenture.com>
2018-05-09 17:19:02 +0530

    1st module

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var debug = require('debug')('app');
 var morgan = require('morgan');
+var path = require('path');
 var chalk = require('chalk');
 
 var app = express();
 
 app.use(morgan('tiny'));
+app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', function(req, res){
-    res.sendFile(__dirname,'/views/index.html');
+    res.sendFile(path.join(__dirname,'/views/index.html'));
 })
 
 app.listen(3000, function(){
