899d9b6997414592f3d0cacca3512139b208ce12
varsha nagaraj <varsha.m.nagaraj@accenture.com>
2018-05-14 09:44:49 +0530

    Module 6

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-var express = require('express');
+const express = require('express');
 var debug = require('debug')('app');
 var morgan = require('morgan');
 var path = require('path');
 var chalk = require('chalk');
 
 var app = express();
+const port = process.env.PORT || 3000;
+const bookRouter = require('./src/routes/bookRoutes');
 
 app.use(morgan('tiny'));
-app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/public/')));
+app.use('/css', express.static(path.join(__dirname, '/node_modules/bootstrap/dist/css')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/bootstrap/js')));
+app.use('/js', express.static(path.join(__dirname, '/node_modules/jquery/dist')));
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
-app.get('/', function(req, res){
-    res.sendFile(path.join(__dirname,'/views/index.html'));
-})
+ 
 
-app.listen(3000, function(){
-    console.log('Listening on port 3000...');
-})
+
+app.use('/books', bookRouter)
+app.get('/', (req, res) => {
+    res.render(
+        'index',
+        { 
+         nav: [{link:'/books', title:'Books'},
+         {link: '/authors', title:'Authors'}],
+         title: 'Library'
+        }
+    );
+});
+
+app.listen(port, () =>
+    console.log('Listening on port 3000...'));
---
+++ src/routes/bookRoutes.js
@@ -0,0 +1,92 @@
+const express = require('express');
+const bookRouter = express.Router();
+
+
+    const books = [{
+            title: 'War and Peace',
+            genre: 'Historical Fiction',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
+        },
+        {
+            title: 'Les Misérables',
+            genre: 'Historical Fiction',
+            author: 'Victor Hugo',
+            read: false
+        },
+        {
+            title: 'The Time Machine',
+            genre: 'Science Fiction',
+            author: 'H. G. Wells',
+            read: false
+        },
+        {
+            title: 'A Journey into the Center of the Earth',
+            genre: 'Science Fiction',
+            author: 'Jules Verne',
+            read: false
+        },
+        {
+            title: 'The Dark World',
+            genre: 'Fantasy',
+            author: 'Henry Kuttner',
+            read: false
+        },
+        {
+            title: 'The Wind in the Willows',
+            genre: 'Fantasy',
+            author: 'Kenneth Grahame',
+            read: false
+        },
+        {
+            title: 'Life On The Mississippi',
+            genre: 'History',
+            author: 'Mark Twain',
+            read: false
+        },
+        {
+            title: 'Childhood',
+            genre: 'Biography',
+            author: 'Lev Nikolayevich Tolstoy',
+            read: false
+        }
+    ];
+
+    bookRouter.route('/')
+        .get((req, res) =>
+            res.render(
+                'bookListView', {
+                    nav: [{
+                            link: '/books',
+                            title: 'Books'
+                        },
+                        {
+                            link: '/authors',
+                            title: 'Authors'
+                        }
+                    ],
+                    title: 'Library',
+                    books
+                }));
+
+    bookRouter.route('/:id')
+        .get((req, res) => {
+        const { id } = req.params.id;
+          res.render(
+          'bookView', {
+              nav: [{
+                      link: '/books',
+                      title: 'Books'
+                  },
+                  {
+                      link: '/authors',
+                      title: 'Authors'
+                  }
+              ],
+              title: 'Library',
+              books: books[id]
+          });
+        }
+
+    
+module.exports = bookRouter;
